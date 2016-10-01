69310009af1fadd41df868bd2079f7ee8b37bb5a
jscherba <jacob.c.scherba@gmail.com>
2016-09-30 20:03:53 -0400

    Create anagram.js

---
+++ anagram.js
@@ -0,0 +1,30 @@
+// Created by Jacob C. Scherba
+// September 30, 2016
+
+function CheckAnagram(){
+
+	var wordOne = document.getElementbyID("wordOne").value,
+		wordTwo = document.getElementbyID("wordTwo").value;
+
+	function checkBlank(){
+		if(wordOne === '' || wordTwo === ''){
+			alert("Whoops! Please enter two strings.");
+			return false;
+		}
+	}
+
+	function checkValid(){
+		if (/[a-zA-Z]/.test(wordOne) == false || /[a-zA-Z]/.test(wordTwo) == false) {
+  			alert("Please only enter letters in your strings!");
+  			return false;
+		}
+	}
+
+	if(checkBlank && checkValid){
+
+		if(wordOne.split("").sort().join("") === wordTwo.split("").sort().join("")){
+			alert("These strings are anagrams!");
+      return true;
+		}
+	}
+}
