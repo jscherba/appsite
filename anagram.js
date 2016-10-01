c900c9f7c089a6f805d8f877083fa21d836c0bf6
jscherba <jacob.c.scherba@gmail.com>
2016-09-30 20:58:17 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -6,25 +6,25 @@ function CheckAnagram(){
 	var wordOne = document.getElementbyID("wordOne").value,
 		wordTwo = document.getElementbyID("wordTwo").value;
 
-	function checkBlank(){
-		if(wordOne === '' || wordTwo === ''){
-			alert("Whoops! Please enter two strings.");
+	
+	if(wordOne === '' || wordTwo === ''){
+		alert("Whoops! Please enter two strings.");
+		return false;
+	}
+	
+
+	
+	else if (/[a-zA-Z]/.test(wordOne) == false || /[a-zA-Z]/.test(wordTwo) == false) {
+			alert("Please only enter letters in your strings!");
 			return false;
-		}
 	}
+	
 
-	function checkValid(){
-		if (/[a-zA-Z]/.test(wordOne) == false || /[a-zA-Z]/.test(wordTwo) == false) {
-  			alert("Please only enter letters in your strings!");
-  			return false;
-		}
-	}
-
-	if(checkBlank && checkValid){
+	else {
 
 		if(wordOne.split("").sort().join("") === wordTwo.split("").sort().join("")){
-			console.log("These strings are anagrams!");
-      			return true;
+			alert("These strings are anagrams!");
+			return true;
 		}
 	}
 }
