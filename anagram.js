dc8cc2c306c92409e2d5621d84f4c8fab5a91f92
jscherba <jacob.c.scherba@gmail.com>
2016-10-01 20:02:58 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -3,21 +3,20 @@
 
 function CheckAnagram(){
 
-	var wordOne = document.getElementById("wordOne").value,
-		wordTwo = document.getElementById("wordTwo").value;
+	var topWord = document.getElementById("wordOne").value,
+		bottomWord = document.getElementById("wordTwo").value;
 
-	console.log(wordOne);
-	console.log(wordTwo);
+	console.log(topWord);
+	console.log(bottomWord);
 	
-	if(wordOne.value === '' || wordTwo.value === ''){
+	if(topWord === '' || bottomWord === ''){
 		document.getElementById("answer").value = "Whoops! Please enter two strings.";
-		console.log("Whoops");
 		return false;
 	}
 	
 
 	
-	else if (/[a-zA-Z]/.test(wordOne.value) == false || /[a-zA-Z]/.test(wordTwo.value) == false) {
+	else if (/[a-zA-Z]/.test(topWord) == false || /[a-zA-Z]/.test(bottomWord) == false) {
 			document.getElementById("answer").value = "Please only enter letters in your strings!";
 			return false;
 	}
@@ -25,7 +24,7 @@ function CheckAnagram(){
 
 	else {
 
-		if(wordOne.value.split("").sort().join("") === wordTwo.value.split("").sort().join("")){
+		if(topWord.value.split("").sort().join("") === bottomWord.value.split("").sort().join("")){
 			document.getElementById("answer").value = "These strings are anagrams!";
 			console.log(document.getElementById("answer").value);
 			return true;
